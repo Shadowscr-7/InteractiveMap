b78130a95fc977558843e3888341a488f755334a
= <jgomez@glp.riogas.com.uy>
2024-12-09 12:37:19 -0300

    Next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false, // Desactiva el branding de Vercel
@@ -11,30 +10,6 @@ const nextConfig: NextConfig = {
         destination: "https://www.riogas.uy/puestos2/rest/ImportarOSM/:path*", // Ruta base del backend
       },
     ];
-  },
-
-  webpack: (config, { isServer }) => {
-    // Ignorar las rutas UNC en Watchpack para evitar errores de watch y escaneo
-    config.watchOptions = {
-      ignored: [
-        path.resolve("\\\\perfilesterminalserveralias.glp.riogas.com.uy"),
-      ],
-    };
-
-    // Desactivar la caché de Webpack si es necesario
-    config.cache = false;
-
-    // Configuración opcional para resolver problemas de módulos en rutas UNC
-    config.resolve = {
-      ...config.resolve,
-      symlinks: false, // Evita problemas al seguir enlaces simbólicos
-      fallback: {
-        ...config.resolve?.fallback,
-        fs: false, // Evita el uso de módulos del sistema de archivos en el cliente
-      },
-    };
-
-    return config;
   },
 };
 
